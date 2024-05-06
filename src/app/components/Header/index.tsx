6e34c3b2b5c3a8e5f698bc5a0128f26b0cc1595c
Luana <luana.smatos01@gmail.com>
2024-05-06 11:48:47 -0300

    Mantendo zoom com localStorage

--- src/app/components/Header/index.tsx
+++ src/app/components/Header/index.tsx
@@ -1,99 +1,183 @@
-"use client"
-import React, { useState } from 'react';
+"use client";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import "./header.css";
 
 let zoomCount = 0;
+let zoom = 1;
 
 const Header = () => {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
+  // menu hamburguer
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-    const toggleMenu = () => {
-        setIsMenuOpen(!isMenuOpen);
-    };
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    const [isMenuOpen1, setIsMenuOpen1] = useState(false);
+  // menu acessibilidade
+  const [isMenuOpen1, setIsMenuOpen1] = useState(false);
 
-    const toggleMenu1 = () => {
-        setIsMenuOpen1(!isMenuOpen1);
-    };
+  const toggleMenu1 = () => {
+    setIsMenuOpen1(!isMenuOpen1);
+  };
 
-    const zoomIncrement = 0.2; // Incremento de zoom desejado em porcentagem
-    // Função para aumentar o zoom
-    function aumentar() {
-        if (zoomCount < 4) {
-            zoomCount++;
-            const currentZoom = parseFloat((document.body.style as any).zoom) || 1; // Type assertion para any para evitar erros de TypeScript
-            const newZoom = currentZoom + zoomIncrement;
-            (document.body.style as any).zoom = `${newZoom}`; // Type assertion para any para evitar erros de TypeScript
-        }
-        if (zoomCount === 4) {
-            zoomCount = 0;
-            (document.body.style as any).zoom = "1"; // Type assertion para any para evitar erros de TypeScript
-        }
+  // função de mudar tamanho do site
+  const zoomIncrement = 0.2;
+
+  function carregarZoom() {
+    const savedZoom = parseFloat(localStorage.getItem("zoom") || "1");
+    const savedZoomCount = parseInt(localStorage.getItem("zoomCount") || "0");
+    zoom = savedZoom;
+    zoomCount = savedZoomCount;
+  }
+
+  function salvarZoom() {
+    localStorage.setItem("zoom", `${zoom}`);
+    localStorage.setItem("zoomCount", `${zoomCount}`);
+  }
+
+  function aplicarZoom() {
+    (document.body.style as any).zoom = `${zoom}`;
+  }
+
+  function aumentar() {
+    if (zoomCount < 3) {
+      zoomCount++;
+      zoom += zoomIncrement;
     }
+    if (zoomCount === 3) {
+      zoomCount = 0;
+      zoom = 1;
+    }
+    salvarZoom();
+    aplicarZoom();
+  }
 
-    return (
-        <>
-            <header className="navbar">
-                <div className="itens-esquerda">
-                    <a href=".">
-                        <Image className="logo-header" src="/logo.png" alt="logo salesforce" width={80} height={40}/>
-                    </a>
-                    <nav className="links-paginas">
-                        <a id='text' className="link-pagina" href="./crm">CRM</a>
-                        <a id='text' className="link-pagina" href="https://salesforcecustomer360.herokuapp.com/quiz/1/">SuperPlataforma</a>
-                        <a id='text' className="link-pagina" href="./produtos">Produtos</a>
-                        <a id='text' className="link-pagina" href="./suporte">Suporte</a>
-                        <a id='text' className="link-pagina" href="./empresa">Empresa</a>
-                    </nav>
-                </div>
-                <button className="acessibilidade" onClick={toggleMenu1}>
-                    <Image src="/acessibilidade1.png" alt="icone de acessibilidade" width={26} height={26}/>
-                    {isMenuOpen1 && (
-                    <div className="botoes-acessibilidade">
-                        <button className='cores'>
-                            <Image src="/cores.svg" alt="cores" width={26} height={26}/>
-                            Cores
-                        </button>
-                        <button className='tamanho' onClick={aumentar}>
-                            <Image src="/texto.svg" alt="texto" width={26} height={26}/>
-                            Tamanho
-                        </button>
-                    </div>
-                    )}
-                </button>
-                <div className="itens-direita">
-                    <button className="icone-pesquisar">
-                        <Image src="/lupa1.png" alt="pesquisar" width={26} height={26}/>
-                    </button>
-                    <button className="icone-login">
-                        <a href="./login">
-                            <Image src="/avatar1.png" alt="login" width={25} height={25}/>
-                        </a>
-                    </button>
-                    <button className="menu-hamburger" onClick={toggleMenu}></button>
-                    {isMenuOpen && (
-                    <div className="menu-dropdown">
-                        <nav className="links-paginas-dropdown">
-                            <a className="link-pagina" href="./crm">CRM</a>
-                            <a className="link-pagina" href="https://salesforcecustomer360.herokuapp.com/quiz/1/">SuperPlataforma</a>
-                            <a className="link-pagina" href="./produtos">Produtos</a>
-                            <a className="link-pagina" href="./suporte">Suporte</a>
-                            <a className="link-pagina" href="./empresa">Empresa</a>
-                        </nav>
-                        <button className="login-dropdown">
-                            <a href="./login">
-                                <Image src="/avatar1.png" alt="login" width={25} height={25}/>
-                                <p>Login</p>
-                            </a>
-                        </button>
-                    </div>
-                    )}
-                </div>
-            </header>
-        </>
-    );
+  window.addEventListener("load", () => {
+    carregarZoom();
+    aplicarZoom();
+  });
+
+  // função de mudar cores do site
+  const cores = {
+    padrao: {
+      corFundo: "#ffffff",
+      corTexto: "#000000",
+    },
+    altoContraste: {
+      corFundo: "#000000",
+      corTexto: "#ffffff",
+    },
+    coresInvertidas: {
+      corFundo: "#ffcccc",
+      corTexto: "#330000",
+    },
+  };
+
+  return (
+    <>
+      <header className="navbar">
+        <div className="itens-esquerda">
+          <a href=".">
+            <Image
+              className="logo-header"
+              src="/logo.png"
+              alt="logo salesforce"
+              width={80}
+              height={40}
+            />
+          </a>
+          <nav className="links-paginas">
+            <a id="text" className="link-pagina" href="./crm">
+              CRM
+            </a>
+            <a
+              id="text"
+              className="link-pagina"
+              href="https://salesforcecustomer360.herokuapp.com/quiz/1/"
+            >
+              SuperPlataforma
+            </a>
+            <a id="text" className="link-pagina" href="./produtos">
+              Produtos
+            </a>
+            <a id="text" className="link-pagina" href="./suporte">
+              Suporte
+            </a>
+            <a id="text" className="link-pagina" href="./empresa">
+              Empresa
+            </a>
+          </nav>
+        </div>
+        <button className="acessibilidade" onClick={toggleMenu1}>
+          <Image
+            src="/acessibilidade1.png"
+            alt="icone de acessibilidade"
+            width={26}
+            height={26}
+          />
+          {isMenuOpen1 && (
+            <div className="botoes-acessibilidade">
+              <button className="cores">
+                <Image src="/cores.svg" alt="cores" width={26} height={26} />
+                Cores
+              </button>
+              <button className="tamanho" onClick={aumentar}>
+                <Image src="/texto.svg" alt="texto" width={26} height={26} />
+                Tamanho
+              </button>
+            </div>
+          )}
+        </button>
+        <div className="itens-direita">
+          <button className="icone-pesquisar">
+            <Image src="/lupa1.png" alt="pesquisar" width={26} height={26} />
+          </button>
+          <button className="icone-login">
+            <a href="./login">
+              <Image src="/avatar1.png" alt="login" width={25} height={25} />
+            </a>
+          </button>
+          <button className="menu-hamburger" onClick={toggleMenu}></button>
+          {isMenuOpen && (
+            <div className="menu-dropdown">
+              <nav className="links-paginas-dropdown">
+                <a className="link-pagina" href="./crm">
+                  CRM
+                </a>
+                <a
+                  className="link-pagina"
+                  href="https://salesforcecustomer360.herokuapp.com/quiz/1/"
+                >
+                  SuperPlataforma
+                </a>
+                <a className="link-pagina" href="./produtos">
+                  Produtos
+                </a>
+                <a className="link-pagina" href="./suporte">
+                  Suporte
+                </a>
+                <a className="link-pagina" href="./empresa">
+                  Empresa
+                </a>
+              </nav>
+              <button className="login-dropdown">
+                <a href="./login">
+                  <Image
+                    src="/avatar1.png"
+                    alt="login"
+                    width={25}
+                    height={25}
+                  />
+                  <p>Login</p>
+                </a>
+              </button>
+            </div>
+          )}
+        </div>
+      </header>
+    </>
+  );
 };
 
 export default Header;

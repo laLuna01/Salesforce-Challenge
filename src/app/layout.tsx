43433508d7d9ec52a1bcab8a58043e481694c896
Luana <luana.smatos01@gmail.com>
2024-05-06 10:26:15 -0300

    Corrigindo cores

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import "../../public/clickCapture";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +20,6 @@ export default function RootLayout({
     <html lang="pt-br">
       <body className={inter.className}>
         <Header/>
-        <script src="./clickCapture.js" defer></script>
         {children}
         <script src="./chatbot.js" defer></script>
         <Footer/>

43da879afc81f555da8a643335efeeaa23b318f5
Luana <luana.smatos01@gmail.com>
2024-05-16 11:07:36 -0300

    função de envio de dados no cadastro de empresa

--- src/app/novaempresa/page.tsx
+++ src/app/novaempresa/page.tsx
@@ -1,41 +1,89 @@
+"use client";
+import { useState, useEffect } from "react";
 import "./novaempresa.css";
  
 export default function NovaEmpresa() {
+    
+    const [nome, setNome] = useState("")
+    const [setor, setSetor] = useState("")
+    const [cnpj, setCnpj] = useState("")
+    const [tamanho, setTamanho] = useState("")
+    const [brasileira, setBrasileira] = useState("")
+
+    const changeNome = (event: any)=> {
+        setNome(event.target.value)
+    }
+    const changeSetor = (event: any)=> {
+        setSetor(event.target.value)
+    }
+    const changeCnpj = (event: any)=> {
+        setCnpj(event.target.value)
+    }
+    const changeTamanho = (event: any)=> {
+        setTamanho(event.target.value)
+    }
+    const changeBrasileira = (event: any)=> {
+        setBrasileira(event.target.value)
+    }
+
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+
+    const enviarDados = async (event: any) => {
+        event.preventDefault();
+
+        const dadosEmpresa = {
+            nome,
+            setor,
+            cnpj,
+            tamanho,
+            brasileira: Boolean(brasileira)
+        };
+        
+        console.log(JSON.stringify(dadosEmpresa));
+
+        const response = await fetch("http://localhost:8080/empresa", {
+            method: "POST",
+            headers: myHeaders,
+            body: JSON.stringify(dadosEmpresa)
+        });
+    };
+
     return (
         <>
             <section className="page">
-                <form className="form" action="">
+                <form className="form">
                     <h2 className="t-form">Cadastrar Empresa</h2>
-                    <input className="input-novaempresa" id="nome" type="text" placeholder="Nome" required />
-                    <input className="input-novaempresa" id="setor" type="text" placeholder="Setor" required />
-                    <input className="input-novaempresa" id="cnpj" type="text" placeholder="CNPJ" required />
+                    <input value={nome} onChange={changeNome} className="input-novaempresa" id="nome" type="text" placeholder="Nome" required />
+                    <input value={setor} onChange={changeSetor} className="input-novaempresa" id="setor" type="text" placeholder="Setor" required />
+                    <input value={cnpj} onChange={changeCnpj} className="input-novaempresa" id="cnpj" type="text" placeholder="CNPJ" required />
                     <p className="p-checkbox">Tamanho da empresa:</p>
                     <div className="checkbox">
                         <div className="checkbox-item">
-                            <input type="radio" name="tamanho" id="pequena" required />
+                            <input value="Pequena" onChange={changeTamanho} type="radio" name="tamanho" id="pequena" required />
                             <label htmlFor="pequena">Pequena</label>
                         </div>
                         <div className="checkbox-item">
-                            <input type="radio" name="tamanho" id="media" required />
+                            <input value="Média" onChange={changeTamanho} type="radio" name="tamanho" id="media" required />
                             <label htmlFor="media">Média</label>
                         </div>
                         <div className="checkbox-item">
-                            <input type="radio" name="tamanho" id="grande" required />
+                            <input value="Grande" onChange={changeTamanho} type="radio" name="tamanho" id="grande" required />
                             <label htmlFor="grande">Grande</label>
                         </div>
                     </div>
                     <p className="p-checkbox">A empresa é brasileira?</p>
                     <div className="checkbox">
                         <div className="checkbox-item">
-                            <input type="radio" name="brasileira" id="brasileira" required />
+                            <input value="true" onChange={changeBrasileira} type="radio" name="brasileira" id="brasileira" required />
                             <label htmlFor="cliente">Sim</label>
                         </div>
                         <div className="checkbox-item">
-                            <input type="radio" name="brasileira" id="estrangeira" required />
+                            <input value="false" onChange={changeBrasileira} type="radio" name="brasileira" id="estrangeira" required />
                             <label htmlFor="atendente">Não</label>
                         </div>
                     </div>
-                    <button className="botao-cadastrar" type="submit">Cadastrar</button>
+                    <button className="botao-cadastrar" type="submit" onClick={enviarDados}>Cadastrar</button>
                     <p className="voltar-p">Empresa já cadastrada? <a href="./cadastro">Voltar</a> </p>
                 </form>
             </section>
